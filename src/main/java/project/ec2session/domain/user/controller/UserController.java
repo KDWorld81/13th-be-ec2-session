b6196b8bd7299e80cea6b6cd8afd884ab7ddcba5
DaeYoung0726 <pdy000726@naver.com>
2025-05-25 01:41:57 +0900

    docs: Swagger 작성

--- src/main/java/project/ec2session/domain/user/controller/UserController.java
+++ src/main/java/project/ec2session/domain/user/controller/UserController.java
@@ -10,25 +10,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.ec2session.common.auth.CustomUserDetails;
+import project.ec2session.domain.user.api.UserApi;
 import project.ec2session.domain.user.dto.UserReq;
 import project.ec2session.domain.user.service.UserService;
 
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
-public class UserController {
+public class UserController implements UserApi {
     private final UserService userService;
 
+    @Override
     @GetMapping("/me")
     public ResponseEntity<?> getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
         return ResponseEntity.ok(userService.readById(userDetails.getUserId()));
     }
 
+    @Override
     @GetMapping
     public ResponseEntity<?> getAllUser() {
         return ResponseEntity.ok(userService.readAll());
     }
 
+    @Override
     @PutMapping
     public ResponseEntity<?> updateUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails,
                                             @RequestBody @Valid UserReq.UpdateInfo request) {
